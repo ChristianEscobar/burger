34ba2b1a7813da68ff4d8ab16c582a7e0993314e
Christian Escobar <crs.escobar@gmail.com>
2018-02-22 11:14:46 -0800

    initial checking of handlebar directory structure

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-//app.use(express.static(path.join(__dirname, "app/public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes
 app.use("/", routes);
