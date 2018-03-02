7cc1519445a2cae8bc5ec35e12779861910e916a
Christian Escobar <crs.escobar@gmail.com>
2018-03-01 18:38:58 -0800

    required textarea input and controller comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,30 +3,40 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
+// Get all burgers
 router.get("/", (req, res) => {
 	burger.selectAll()
 		.then((result) => {
+			// Create object for view
 			const burgersObj = {
 				burgers: result[0]
 			};
+
+			// Display view
 			res.render("index", burgersObj);
 		}).catch((error) => {
 			console.error(error);
 		});
 });
 
+// Add a new burger
 router.post("/", (req, res) => {
 	burger.insertOne(req.body.burgerDescription, false)
 		.then((result) => {
+
+			// Reload the index view
 			res.redirect("/");
 		}).catch((error) => {
 			console.error(error);
 		});
 });
 
+// Update a specific burger
 router.put("/:id", (req, res) => {
 	burger.updateOne(req.params.id)
 		.then((result) => {
+
+			// Reload the index view
 			res.redirect("/");
 		}).catch((error) => {
 			console.error(error);
